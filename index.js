260d8ee587e453ad4e42341086b174f4c80ca334
Haniel2607 <hanielschramm@gmail.com>
2024-09-17 21:46:23 -0300

    atualização

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //configuração inicial
-const express = require('express');
-const { default: mongoose } = require('mongoose');
+const express = require('express')
 const app = express()
+const mongoose = require("mongoose")
+const Person = require("./models/Person");
 
-app.listen(3000)
 app.use(
     express.urlencoded({
         extended: true
@@ -16,6 +16,39 @@ app.use(express.json())
 app.get('/', (req, res)=>{
     res.json({message: "rodou"})
 })
+
+
+//Create
+app.post("/person", async (req, res) =>{
+    const {name, salary, approved} = req.body;
+
+    const person = {
+        name,
+        salary,
+        approved,
+    }
+
+    try{
+        await Person.create(person)
+        res.status(201).json({message: "Pessoa inserida no sistema com sucesso!"})
+    } catch (error){
+        res.status(500).json({erro: error})
+    }
+})
+
+// Read
+app.get("/person", async (req, res) => {
+    try {
+        const people = await Person.find()
+        res.status(200).json(people);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+})
+
+//Update
+
+//Delete
 
 mongoose.connect("mongodb://localhost:27017").then(()=>{
     console.log("Conectado!")

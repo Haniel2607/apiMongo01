49e4373b7ab59ed3483a77e8213b8409a221899c
Haniel2607 <hanielschramm@gmail.com>
2024-09-24 21:13:15 -0300

    completo

--- index.js
+++ index.js
@@ -35,19 +35,74 @@ app.post("/person", async (req, res) =>{
     }
 })
 
-// Read
+//Read
 app.get("/person", async (req, res) => {
     try {
-        const people = await Person.find()
-        res.status(200).json(people);
+        const peaple = await Person.find()
+        res.status(200).json(peaple);
     } catch (error) {
         res.status(500).json({ error: error });
     }
 })
 
-// Update
+//Read by id
+app.get("/person/:id", async (req, res)=>{
+    const id = req.params.id
+    try{
+        const peaple = await Person.findOne({_id: id})
 
-// Delete
+        if(!peaple){
+            res.status(422).json({message: "Usuário não encontrado!"})
+            return
+        }
+        res.status(200).json(peaple)
+    } catch(error){
+        res.status(500).json({erro: error})
+    }
+})
+
+//Update
+app.patch("/person/:id", async (req, res)=>{
+    const id = req.params.id
+
+    const {name, salary, approved} = req.body
+
+    const person = {
+        name,
+        salary,
+        approved,
+    }
+    try{
+        const updatePerson = await Person.updateOne({_id: id}, person)
+
+        if(updatePerson.matchedCount === 0){
+            res.status(422).json({message: "Usuário não encontrado!"})
+            return
+        }
+        res.status(200).json(person)
+    } catch(error){
+        res.status(500).json({erro: error})
+    }
+})
+
+//Delete
+app.delete("/person/:id", async (req, res)=>{
+    const id = req.params.id
+
+    const person = await Person.findOne({_id: id})
+
+    if (!person) {
+        res.status(422).json({ message: "Usuário não encontrado!" });
+        return;
+    }
+
+    try{
+        await Person.deleteOne({_id: id})
+        res.status(200).json({message: "Usuário deletado com sucesso!"})
+    }catch(error){
+        res.status(500).json({erro: error})
+    }
+})
 
 mongoose.connect("mongodb://localhost:27017").then(()=>{
     console.log("Conectado!")
